75e8e578118bdd39370217f9197083a25d3aba3a
Anurag Joshi <anurag88787@gmail.com>
2024-06-14 23:33:45 +0530

    feat: integrate backend with frontend for displaying notes
    - Implemented functionality to fetch and display notes from the backend
    - Updated the NoteView to dynamically load note data

--- lib/archieveView.dart
+++ lib/archieveView.dart
@@ -77,205 +77,203 @@ class _ArchieveViewState extends State<ArchieveView> {
       drawer: SideMenu(),
       backgroundColor: bgColor,
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                width: MediaQuery.of(context).size.width,
-                height: 55,
-                decoration: BoxDecoration(
-                  color: cardColor,
-                  borderRadius: BorderRadius.circular(15),
-                  boxShadow: [
-                    BoxShadow(
-                      color: black.withOpacity(0.2),
-                      spreadRadius: 1,
-                      blurRadius: 3,
-                    ),
-                  ],
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        IconButton(
-                          onPressed: () {
-                            _drawerKey.currentState!.openDrawer();
-                          },
-                          icon: Icon(
-                            Icons.menu,
-                            color: white,
-                          ),
-                        ),
-                        SizedBox(width: 16),
-                        Container(
-                          height: 55,
-                          width: 200,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "Search your notes",
-                                style: TextStyle(
-                                  color: white.withOpacity(0.5),
-                                  fontSize: 16,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        TextButton(
-                          onPressed: () {},
-                          child: Icon(
-                            Icons.grid_view,
-                            color: white,
-                          ),
-                          style: ButtonStyle(
-                            overlayColor: MaterialStateColor.resolveWith(
-                                  (states) => white.withOpacity(0.1),
-                            ),
-                            shape: MaterialStateProperty.all<
-                                RoundedRectangleBorder>(
-                              RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(50.0),
-                              ),
-                            ),
-                          ),
-                        ),
-                        SizedBox(width: 9),
-                        CircleAvatar(
-                          radius: 16,
-                          backgroundColor: Colors.orange,
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                alignment: Alignment.centerLeft,
-                padding: EdgeInsets.symmetric(horizontal: 10),
-                margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-                child: Text(
-                  "ALL",
-                  style: TextStyle(
-                    color: white.withOpacity(0.5),
-                    fontSize: 13,
-                    fontWeight: FontWeight.bold,
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+              width: MediaQuery.of(context).size.width,
+              height: 55,
+              decoration: BoxDecoration(
+                color: cardColor,
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    color: black.withOpacity(0.2),
+                    spreadRadius: 1,
+                    blurRadius: 3,
                   ),
-                ),
+                ],
               ),
-              Container(
-                padding:
-                const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-                child: MasonryGridView.count(
-                  physics: const NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 12,
-                  crossAxisSpacing: 12,
-                  itemCount: notesList.length,
-                  itemBuilder: (context, index) {
-                    return InkWell(
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => NoteView(
-                                  note: notesList[index],
-                                )));
-                      },
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: cardColor,
-                          borderRadius: BorderRadius.circular(8),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black.withOpacity(0.2),
-                              spreadRadius: 1,
-                              blurRadius: 3,
-                            ),
-                          ],
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          _drawerKey.currentState!.openDrawer();
+                        },
+                        icon: Icon(
+                          Icons.menu,
+                          color: white,
                         ),
-                        padding: const EdgeInsets.all(10.0),
+                      ),
+                      SizedBox(width: 16),
+                      Container(
+                        height: 55,
+                        width: 200,
                         child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             Text(
-                              notesList[index].title,
-                              style: const TextStyle(
-                                fontSize: 18.0,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
-                              ),
-                            ),
-                            SizedBox(height: 10.0),
-                            Text(
-                              notesList[index].content,
-                              style: const TextStyle(
-                                fontSize: 14.0,
-                                color: Colors.white,
+                              "Search your notes",
+                              style: TextStyle(
+                                color: white.withOpacity(0.5),
+                                fontSize: 16,
                               ),
                             ),
                           ],
                         ),
                       ),
-                    );
-                  },
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      TextButton(
+                        onPressed: () {},
+                        child: Icon(
+                          Icons.grid_view,
+                          color: white,
+                        ),
+                        style: ButtonStyle(
+                          overlayColor: MaterialStateColor.resolveWith(
+                                (states) => white.withOpacity(0.1),
+                          ),
+                          shape: MaterialStateProperty.all<
+                              RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50.0),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 9),
+                      CircleAvatar(
+                        radius: 16,
+                        backgroundColor: Colors.orange,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+              child: Text(
+                "ALL",
+                style: TextStyle(
+                  color: white.withOpacity(0.5),
+                  fontSize: 13,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-              ListView.builder(
+            ),
+            Container(
+              padding:
+              const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+              child: MasonryGridView.count(
                 physics: const NeverScrollableScrollPhysics(),
                 shrinkWrap: true,
+                crossAxisCount: 2,
+                mainAxisSpacing: 12,
+                crossAxisSpacing: 12,
                 itemCount: notesList.length,
                 itemBuilder: (context, index) {
-                  return Container(
-                    margin: EdgeInsets.all(10),
-                    padding: const EdgeInsets.all(10.0),
-                    decoration: BoxDecoration(
-                      color: Color(0xFF34A853),
-                      borderRadius: BorderRadius.circular(8),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.2),
-                          spreadRadius: 1,
-                          blurRadius: 3,
-                        ),
-                      ],
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          notesList[index].title,
-                          style: const TextStyle(
-                            fontSize: 18.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
+                  return InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => NoteView(
+                                note: notesList[index],
+                              )));
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: cardColor,
+                        borderRadius: BorderRadius.circular(8),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.2),
+                            spreadRadius: 1,
+                            blurRadius: 3,
                           ),
-                        ),
-                        SizedBox(height: 10.0),
-                        Text(
-                          notesList[index].content,
-                          style: const TextStyle(
-                            fontSize: 14.0,
-                            color: Colors.white,
+                        ],
+                      ),
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            notesList[index].title,
+                            style: const TextStyle(
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
                           ),
-                        ),
-                      ],
+                          SizedBox(height: 10.0),
+                          Text(
+                            notesList[index].content,
+                            style: const TextStyle(
+                              fontSize: 14.0,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   );
                 },
               ),
-            ],
-          ),
+            ),
+            ListView.builder(
+              physics: const NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              itemCount: notesList.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  margin: EdgeInsets.all(10),
+                  padding: const EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    color: Color(0xFF34A853),
+                    borderRadius: BorderRadius.circular(8),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.2),
+                        spreadRadius: 1,
+                        blurRadius: 3,
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        notesList[index].title,
+                        style: const TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(height: 10.0),
+                      Text(
+                        notesList[index].content,
+                        style: const TextStyle(
+                          fontSize: 14.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ],
         ),
       ),
     );
