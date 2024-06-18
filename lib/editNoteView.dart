419970663973f238c03362a21f9ba0627bf67c30
Anurag Joshi <anurag88787@gmail.com>
2024-06-18 13:43:32 +0530

    feat: Enhance EditNoteView with updated UI and dynamic note saving
    - Refactored to use TextEditingController for title and content
    - Implemented save functionality to update note in database
    - Improved usability with streamlined UI and consistent styling

--- lib/editNoteView.dart
+++ lib/editNoteView.dart
@@ -1,16 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:clipnote/noteView.dart';
-
+import 'package:clipnote/model/myNoteModel.dart';
+import 'package:clipnote/services/db.dart';
 import 'colors.dart';
 
-class Editnoteview extends StatefulWidget {
-  const Editnoteview({super.key});
+class EditNoteView extends StatefulWidget {
+  final Note note;
+
+  EditNoteView({required this.note});
 
   @override
-  State<Editnoteview> createState() => _EditnoteviewState();
+  _EditNoteViewState createState() => _EditNoteViewState();
 }
 
-class _EditnoteviewState extends State<Editnoteview> {
+class _EditNoteViewState extends State<EditNoteView> {
+  late TextEditingController _titleController;
+  late TextEditingController _contentController;
+
+  @override
+  void initState() {
+    super.initState();
+    _titleController = TextEditingController(text: widget.note.title);
+    _contentController = TextEditingController(text: widget.note.content);
+  }
+
+  Future<void> _saveNote() async {
+    final updatedNote = widget.note.copy(
+      title: _titleController.text,
+      content: _contentController.text,
+    );
+    await NotesDatabase.instance.updateNote(updatedNote);
+    Navigator.pop(context, updatedNote);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,10 +41,10 @@ class _EditnoteviewState extends State<Editnoteview> {
         backgroundColor: bgColor,
         actions: [
           IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.save_outlined),
             splashRadius: 18,
-          )
+            onPressed: _saveNote,
+            icon: Icon(Icons.save_outlined, color: white),
+          ),
         ],
       ),
       body: Container(
@@ -32,43 +53,49 @@ class _EditnoteviewState extends State<Editnoteview> {
           children: [
             TextField(
               cursorColor: white,
+              controller: _titleController,
               style: TextStyle(
-                  fontSize: 25, color: white, fontWeight: FontWeight.bold),
+                fontSize: 25,
+                color: white,
+                fontWeight: FontWeight.bold,
+              ),
               decoration: InputDecoration(
+                border: InputBorder.none,
+                focusedBorder: InputBorder.none,
+                enabledBorder: InputBorder.none,
+                errorBorder: InputBorder.none,
+                disabledBorder: InputBorder.none,
+                hintText: "Title",
+                hintStyle: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey.withOpacity(0.8),
+                ),
+              ),
+            ),
+            Expanded(
+              child: TextField(
+                controller: _contentController,
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                cursorColor: white,
+                style: TextStyle(
+                  fontSize: 17,
+                  color: white,
+                ),
+                decoration: InputDecoration(
                   border: InputBorder.none,
                   focusedBorder: InputBorder.none,
                   enabledBorder: InputBorder.none,
                   errorBorder: InputBorder.none,
                   disabledBorder: InputBorder.none,
-                  hintText: "Title",
+                  hintText: "Note",
                   hintStyle: TextStyle(
                     fontWeight: FontWeight.bold,
                     color: Colors.grey.withOpacity(0.8),
-                  )),
-            ),
-            Container(
-                height: 300,
-                child: TextField(
-                  keyboardType: TextInputType.multiline,
-                  minLines: 50,
-                  maxLines: null,
-                  cursorColor: white,
-                  style: TextStyle(
-                    fontSize: 17,
-                    color: white,
                   ),
-                  decoration: InputDecoration(
-                      border: InputBorder.none,
-                      focusedBorder: InputBorder.none,
-                      enabledBorder: InputBorder.none,
-                      errorBorder: InputBorder.none,
-                      disabledBorder: InputBorder.none,
-                      hintText: "Note",
-                      hintStyle: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.grey.withOpacity(0.8),
-                      )),
-                )),
+                ),
+              ),
+            ),
           ],
         ),
       ),
