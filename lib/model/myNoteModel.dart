67a4389ed69fb1248d5724aa09ad646f83af5897
Anurag Joshi <anurag88787@gmail.com>
2024-06-18 23:03:45 +0530

    feat: Add isArchieve field to Note model
    - Introduced 'isArchieve' boolean field to the Note model for archive functionality.
    - Updated NoteFields to include 'isArchieve'.
    - Modified fromJson and toJson methods to handle the new 'isArchieve' field.
    - Adjusted copy method to support 'isArchieve' field.

--- lib/model/myNoteModel.dart
+++ lib/model/myNoteModel.dart
@@ -1,61 +1,75 @@
-  class Note {
-    final int? id;
-    final bool pin;
-    final String title;
-    final String content;
-    final DateTime createdTime;
+class Note {
+  final int? id;
+  final bool pin;
+  final bool isArchieve;
+  final String title;
+  final String content;
+  final DateTime createdTime;
 
-    const Note({
-      this.id,
-      required this.pin,
-      required this.title,
-      required this.content,
-      required this.createdTime,
-    });
+  Note({
+    this.id,
+    required this.pin,
+    required this.isArchieve,
+    required this.title,
+    required this.content,
+    required this.createdTime,
+  });
 
-    Note copy({
-      int? id,
-      bool? pin,
-      String? title,
-      String? content,
-      DateTime? createdTime,
-    }) {
-      return Note(
-        id: id ?? this.id,
-        pin: pin ?? this.pin,
-        title: title ?? this.title,
-        content: content ?? this.content,
-        createdTime: createdTime ?? this.createdTime,
-      );
-    }
-
-    static Note fromJson(Map<String, Object?> json) {
-      return Note(
-        id: json[NoteFields.id] as int?,
-        pin: json[NoteFields.pin] == 1,
-        title: json[NoteFields.title] as String,
-        content: json[NoteFields.content] as String,
-        createdTime: DateTime.parse(json[NoteFields.createdTime] as String),
-      );
-    }
-
-    Map<String, Object?> toJson() {
-      return {
-        NoteFields.id: id,
-        NoteFields.pin: pin ? 1 : 0,
-        NoteFields.title: title,
-        NoteFields.content: content,
-        NoteFields.createdTime: createdTime.toIso8601String(),
-      };
-    }
+  Note copy({
+    int? id,
+    bool? pin,
+    bool? isArchieve,
+    String? title,
+    String? content,
+    DateTime? createdTime,
+  }) {
+    return Note(
+      id: id ?? this.id,
+      pin: pin ?? this.pin,
+      isArchieve: isArchieve ?? this.isArchieve,
+      title: title ?? this.title,
+      content: content ?? this.content,
+      createdTime: createdTime ?? this.createdTime,
+    );
   }
 
-  class NoteFields {
-    static final List<String> values = [id, pin, title, content, createdTime];
-
-    static const String id = '_id';
-    static const String pin = 'pin';
-    static const String title = 'title';
-    static const String content = 'content';
-    static const String createdTime = 'createdTime';
+  static Note fromJson(Map<String, Object?> json) {
+    return Note(
+      id: json[NoteFields.id] as int?,
+      pin: json[NoteFields.pin] == 1,
+      isArchieve: json[NoteFields.isArchieve] == 1,
+      title: json[NoteFields.title] as String,
+      content: json[NoteFields.content] as String,
+      createdTime: DateTime.parse(json[NoteFields.createdTime] as String),
+    );
   }
+
+  Map<String, Object?> toJson() {
+    return {
+      NoteFields.id: id,
+      NoteFields.pin: pin ? 1 : 0,
+      NoteFields.isArchieve: isArchieve ? 1 : 0,
+      NoteFields.title: title,
+      NoteFields.content: content,
+      NoteFields.createdTime: createdTime.toIso8601String(),
+    };
+  }
+}
+
+class NoteFields {
+  static final List<String> values = [
+    id,
+    pin,
+    isArchieve,
+    title,
+    content,
+    createdTime
+  ];
+
+  static const String id = '_id';
+  static const String pin = 'pin';
+  static const String isArchieve = 'isArchieve';
+  static const String title = 'title';
+  static const String content = 'content';
+  static const String createdTime = 'createdTime';
+}
