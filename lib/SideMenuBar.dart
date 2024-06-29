f8058c72df5b2c20570e09b0773f0032ba72e8b8
Anurag Joshi <anurag88787@gmail.com>
2024-06-29 17:58:28 +0530

    Refactor SideMenu: Enhance UI consistency and introduce color customization
    - Updated UI elements in SideMenu to improve visual consistency.
    - Introduced a color customization feature for selected menu items.
    - Optimized code structure for better maintainability and readability.

--- lib/SideMenuBar.dart
+++ lib/SideMenuBar.dart
@@ -2,33 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:clipnote/archieveView.dart';
 import 'package:clipnote/settingsView.dart';
 import 'package:clipnote/home.dart';
+import 'package:clipnote/colors.dart';
 
 class SideMenu extends StatelessWidget {
-  const SideMenu({Key? key}) : super(key: key);
+  const SideMenu({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: Container(
-        color: Colors.black,
+        color: bgColor,
         child: SafeArea(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Container(
-                margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
                 child: const Row(
                   children: [
                     Icon(
                       Icons.note_alt_outlined,
-                      color: Colors.white,
+                      color: white,
                       size: 30,
                     ),
                     SizedBox(width: 10),
                     Text(
                       "ClipNote",
                       style: TextStyle(
-                        color: Colors.white,
+                        color: white,
                         fontWeight: FontWeight.bold,
                         fontSize: 24,
                       ),
@@ -37,7 +39,7 @@ class SideMenu extends StatelessWidget {
                 ),
               ),
               Divider(
-                color: Colors.white.withOpacity(0.3),
+                color: white.withOpacity(0.3),
               ),
               Expanded(
                 child: ListView(
@@ -47,6 +49,7 @@ class SideMenu extends StatelessWidget {
                       icon: Icons.lightbulb_outline,
                       label: "Notes",
                       destination: const Home(),
+                      selected: true,
                     ),
                     _buildMenuItem(
                       context,
@@ -70,46 +73,40 @@ class SideMenu extends StatelessWidget {
     );
   }
 
-  Widget _buildMenuItem(BuildContext context, {
+  Widget _buildMenuItem(
+    BuildContext context, {
     required IconData icon,
     required String label,
     required Widget destination,
+    bool selected = false,
   }) {
-    final notesColor = Colors.amber; // Use named color for better readability
+    final color =
+        selected ? Colors.orangeAccent.withOpacity(0.7) : Colors.transparent;
 
     return InkWell(
       onTap: () {
-        Navigator.push(context, MaterialPageRoute(builder: (context) => destination));
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => destination));
       },
       child: Container(
         margin: const EdgeInsets.symmetric(vertical: 5),
         padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
         decoration: BoxDecoration(
-          color: label == "Notes" ? notesColor : Colors.grey.shade900,
-          borderRadius: const BorderRadius.only(
-            topRight: Radius.circular(30),
-            bottomRight: Radius.circular(30),
-          ),
+          color: color,
+          borderRadius: BorderRadius.circular(5),
         ),
         child: Row(
           children: [
-            Container(
-              padding: const EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                color: Colors.grey.shade800,
-                shape: BoxShape.circle,
-              ),
-              child: Icon(
-                icon,
-                size: 25,
-                color: Colors.white.withOpacity(0.9),
-              ),
+            Icon(
+              icon,
+              size: 25,
+              color: white.withOpacity(0.9),
             ),
             const SizedBox(width: 20),
             Text(
               label,
               style: TextStyle(
-                color: Colors.white.withOpacity(0.9),
+                color: white.withOpacity(0.9),
                 fontSize: 18,
               ),
             ),
