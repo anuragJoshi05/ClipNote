664fed7d5c2533f60420bf20461b0146d9103ddd
Anurag Joshi <anurag88787@gmail.com>
2024-07-03 13:05:00 +0530

    Update color constants for enhanced theme consistency

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-const bgColor = Color(0xFF0B0B0B); // Another very subtle grey option, primarily black
+const bgColor =
+    Color(0xFF0B0B0B); // Another very subtle grey option, primarily black
 const searchBarColor = Color(0xFF1F1F1F); // Dark grey, closer to black
 const cardColor = Color(0xFF1F1F1F);
 const black = Color(0xFF000000);
