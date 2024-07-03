6d81ca3688170a5b06a6352345d6884e47f566ef
Anurag Joshi <anurag88787@gmail.com>
2024-07-03 12:57:07 +0530

    Refactor CreateNoteview for improved UI and state management

--- lib/createNoteView.dart
+++ lib/createNoteView.dart
@@ -1,9 +1,9 @@
-import 'package:clipnote/home.dart';
-import 'package:clipnote/services/db.dart';
 import 'package:flutter/material.dart';
 import 'colors.dart';
 import 'model/myNoteModel.dart';
 import 'package:uuid/uuid.dart';
+import 'home.dart';
+import 'services/db.dart';
 
 class CreateNoteview extends StatefulWidget {
   const CreateNoteview({super.key});
@@ -16,6 +16,7 @@ class _CreateNoteviewState extends State<CreateNoteview> {
   TextEditingController title = TextEditingController();
   TextEditingController content = TextEditingController();
   var uuid = const Uuid();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,32 +29,47 @@ class _CreateNoteviewState extends State<CreateNoteview> {
             splashRadius: 18,
             onPressed: () async {
               await NotesDatabase.instance.insertEntry(Note(
-                  title: title.text,
-                  uniqueID : uuid.v1(),
-                  content: content.text,
-                  pin: false,
-                  isArchieve: false,
-                  createdTime: DateTime.now()));
+                title: title.text,
+                uniqueID: uuid.v1(),
+                content: content.text,
+                pin: false,
+                isArchieve: false,
+                createdTime: DateTime.now(),
+              ));
 
               Navigator.pushReplacement(
-                  context, MaterialPageRoute(builder: (context) => const Home()));
+                context,
+                MaterialPageRoute(builder: (context) => const Home()),
+              );
             },
             icon: const Icon(Icons.save_outlined),
           )
         ],
       ),
-      body: SingleChildScrollView(
-        physics: const BouncingScrollPhysics(),
-        child: Container(
-          margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-          child: Column(
-            children: [
-              TextField(
-                cursorColor: white,
-                controller: title,
-                style: const TextStyle(
-                    fontSize: 25, color: white, fontWeight: FontWeight.bold),
-                decoration: InputDecoration(
+      body: WillPopScope(
+        onWillPop: () async {
+          // Handle your logic when the Android system back button is pressed
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => const Home()),
+          );
+          return true; // Return true to allow back navigation
+        },
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Container(
+            margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+            child: Column(
+              children: [
+                TextField(
+                  cursorColor: white,
+                  controller: title,
+                  style: const TextStyle(
+                    fontSize: 25,
+                    color: white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  decoration: InputDecoration(
                     border: InputBorder.none,
                     focusedBorder: InputBorder.none,
                     enabledBorder: InputBorder.none,
@@ -63,22 +79,23 @@ class _CreateNoteviewState extends State<CreateNoteview> {
                     hintStyle: TextStyle(
                       fontWeight: FontWeight.bold,
                       color: Colors.grey.withOpacity(0.8),
-                    )),
-              ),
-              const SizedBox(height: 10),
-              Container(
-                height: MediaQuery.of(context).size.height - 150,
-                child: TextField(
-                  controller: content,
-                  keyboardType: TextInputType.multiline,
-                  minLines: 1,
-                  maxLines: null,
-                  cursorColor: white,
-                  style: const TextStyle(
-                    fontSize: 17,
-                    color: white,
+                    ),
                   ),
-                  decoration: InputDecoration(
+                ),
+                const SizedBox(height: 10),
+                Container(
+                  height: MediaQuery.of(context).size.height - 150,
+                  child: TextField(
+                    controller: content,
+                    keyboardType: TextInputType.multiline,
+                    minLines: 1,
+                    maxLines: null,
+                    cursorColor: white,
+                    style: const TextStyle(
+                      fontSize: 17,
+                      color: white,
+                    ),
+                    decoration: InputDecoration(
                       border: InputBorder.none,
                       focusedBorder: InputBorder.none,
                       enabledBorder: InputBorder.none,
@@ -88,10 +105,12 @@ class _CreateNoteviewState extends State<CreateNoteview> {
                       hintStyle: TextStyle(
                         fontWeight: FontWeight.bold,
                         color: Colors.grey.withOpacity(0.8),
-                      )),
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
